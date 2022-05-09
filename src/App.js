8d863b83f968d150845cbbb768318c9d097fa092
Joni Jansson <joni.jansson@saunalahti.fi>
2022-05-09 18:43:22 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-
+// Import React and hooks: useState,
+import React, { useState } from 'react';
 
 function App() {
+
+  // By default shows all items from API
+  const [operation, setOperation] = useState(
+
+
+  )
+
   return (
+    // API Requests
     <>
       <button>Show Items</button>
       <button>Add Item</button>
       <button>Delete Item</button>
       <button>Update Item</button>
       <button>Find Item</button>
+      <br></br>
+      <table></table>
     </>
   )
-
-
 }
 
 export default App;
