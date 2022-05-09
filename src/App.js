3b48c7c716fe209b4eecb6abcfb337eb714e64c5
Joni Jansson <joni.jansson@saunalahti.fi>
2022-05-09 18:19:54 +0300

    React app is set up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <>
+      <button>Show Items</button>
+      <button>Add Item</button>
+      <button>Delete Item</button>
+      <button>Update Item</button>
+      <button>Find Item</button>
+    </>
+  )
+
+
+}
+
+export default App;
